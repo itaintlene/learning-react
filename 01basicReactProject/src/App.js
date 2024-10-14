fc79ba4eba07e28b0e3c2a9eb8e7947b6e718253
itaintlene <105277093+itaintlene@users.noreply.github.com>
2024-10-14 14:26:21 +0000

    react basics

---
+++ 01basicReactProject/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+          <h1>hello world!</h1>
+          <p>we made a react project using `npx create-react-app` command on terminal.</p>
+          <p>but the traditional method can be quite slow so we also learnt to use vite library</p>
+          <p>`npm create vite@latest` creates a basic structure then do `npm install`</p>
+          <p>um what else, we learnt about package.json file</p>
+          <p>then we deleted some random test, css etc files to clear up space and confusion</p>
+    </div>
+  );
+}
+
+export default App;
