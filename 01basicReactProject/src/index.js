95e6b548d72882ba8f5e5ce16db56c5fc19c3732
itaintlene <105277093+itaintlene@users.noreply.github.com>
2024-10-14 15:52:45 +0000

    basics

--- 01basicReactProject/src/index.js
+++ 01basicReactProject/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // reactDOM is like reactNative for browser
 import App from './App';
 
+
+// react makes a virtual DOM and compares to browser DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
