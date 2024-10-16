adff101c1899dc930d73f1d6495b79aa6273e809
itaintlene <105277093+itaintlene@users.noreply.github.com>
2024-10-16 15:54:04 +0000

    custom react

--- 01viteproject/src/App.jsx
+++ 01viteproject/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
 
   return (
     <h1>this react page is made using vite library.</h1>
+    // so here to inject any variables use {}, its called evaluated expression
+    // but you cant declare it inside return() bc react is a framework w set rules and it doesnt
+    // allows declarations etc inside return function.
   )
 }
 
--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App />       
+{/* here App is the function from 'App.jsx' and react bundler is converting it to an html tag */}
+{/* and you can also declare it as a function but thats a bad practice so dont. */}
+{/* also you can to use React.createElement() to inject custom functions here only */}
   </StrictMode>,
 )
